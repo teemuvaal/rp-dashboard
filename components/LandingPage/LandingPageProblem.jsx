095ee916aba3d8cef3a03c5786a86eb6cb28744f
Teemu Vaalasmaa <flipsuk@gmail.com>
2024-08-23 12:07:40 +0300

    landing page adjustments

---
+++ components/LandingPage/LandingPageProblem.jsx
@@ -0,0 +1,26 @@
+export default function LandingPageProblem() {
+    return (
+        <div className="flex flex-row items-center text-center gap-4 text-[#3c2a1e] text-sm">
+            <p
+            className="w-1/4"
+            >
+                Your time running campaigns is spent on the following: write notes, clean up notes and create summaries for your players of past sessions.
+            </p>
+            <p
+            className="w-1/4"
+            >
+                Notes on paper are not easily sharable and time consuming to clean up and upload.
+            </p>
+            <p
+            className="w-1/4"
+            >
+                Chat history is easily cluttered and hard to read. When and where are we playing? Is Mary or John playing?
+            </p>
+            <p
+            className="w-1/4"
+            >
+                Writing compelling summaries is hard and time consuming. It's not easy to summarize multiple sessions to a quick recap that sparks the players interest after a break.
+            </p>
+        </div>
+    )
+}
