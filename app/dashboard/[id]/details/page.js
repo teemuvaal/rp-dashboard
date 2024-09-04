4e8ea7d76108eab00e09b395aef6e880c147a07d
Teemu Vaalasmaa <flipsuk@gmail.com>
2024-09-04 12:30:34 +0300

    improve campaign details form

--- app/dashboard/[id]/details/page.js
+++ app/dashboard/[id]/details/page.js
@@ -1,42 +1,8 @@
-'use client'
+import { fetchCampaignDetails } from '@/app/dashboard/actions'
+import EditableCampaignDetails from '@/components/Dashboard/EditableCampaignDetails'
 
-import { useEffect, useState } from 'react'
-import { fetchCampaignDetails, updateCampaignDetails } from '@/app/dashboard/actions'
-import Button from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Textarea } from '@/components/ui/textarea'
-import { Pen, Pencil, Save } from 'lucide-react'
-
-export default function DetailsPage({ params }) {
-    const [campaign, setCampaign] = useState(null)
-    const [error, setError] = useState(null)
-    const [isEditingName, setIsEditingName] = useState(false)
-    const [isEditingDescription, setIsEditingDescription] = useState(false)
-
-    useEffect(() => {
-        async function loadCampaignDetails() {
-            const result = await fetchCampaignDetails(params.id)
-            if (result.error) {
-                setError(result.error)
-            } else {
-                setCampaign(result.campaign)
-            }
-        }
-
-        loadCampaignDetails()
-    }, [params.id])
-
-    const handleSave = async (field) => {
-        const updates = { [field]: campaign[field] }
-        const result = await updateCampaignDetails(params.id, updates)
-        if (result.error) {
-            setError(result.error)
-        } else {
-            setCampaign(result.campaign)
-        }
-        if (field === 'name') setIsEditingName(false)
-        if (field === 'description') setIsEditingDescription(false)
-    }
+export default async function DetailsPage({ params }) {
+    const { campaign, error } = await fetchCampaignDetails(params.id)
 
     if (error) {
         return <div>Error: {error}</div>
@@ -47,52 +13,8 @@ export default function DetailsPage({ params }) {
     }
 
     return (
-        <div className="w-full border-black border p-4 rounded-sm">
-            <div className="flex flex-col gap-2">
-                {isEditingName ? (
-                    <span className="flex flex-row gap-2 items-center">
-                        <Input 
-                            className="w-[400px]" 
-                            value={campaign.name} 
-                            onChange={(e) => setCampaign({ ...campaign, name: e.target.value })} 
-                        />
-                        <Save 
-                            className="w-4 h-4 cursor-pointer hover:scale-110" 
-                            onClick={() => handleSave('name')} 
-                        />
-                    </span>
-                ) : (
-                    <span className="flex flex-row gap-2 items-center">
-                        <h1>{campaign.name}</h1>
-                        <Pen 
-                            className="w-4 h-4 cursor-pointer hover:scale-110" 
-                            onClick={() => setIsEditingName(true)} 
-                        /> 
-                    </span>
-                )}
-            
-                {isEditingDescription ? (
-                    <span className="flex flex-col gap-2">
-                        <Textarea 
-                            className="w-[400px]" 
-                            value={campaign.description} 
-                            onChange={(e) => setCampaign({ ...campaign, description: e.target.value })} 
-                        />
-                        <Save 
-                            className="w-4 h-4 cursor-pointer hover:scale-110" 
-                            onClick={() => handleSave('description')} 
-                        />
-                    </span>
-                ) : (
-                    <span className="flex flex-row gap-2 items-center">
-                        <p>{campaign.description}</p>
-                        <Pencil 
-                            className="w-4 h-4 cursor-pointer hover:scale-110" 
-                            onClick={() => setIsEditingDescription(true)} 
-                        />
-                    </span>
-                )}
-            </div>
+        <div className="w-full border-gray-200 border-2 h-screen p-4 rounded-sm">
+            <EditableCampaignDetails campaign={campaign} campaignId={params.id} />
         </div>
     )
 }
---
+++ components/Dashboard/EditableCampaignDetails.jsx
@@ -0,0 +1,93 @@
+'use client'
+
+import { useState } from 'react'
+import { updateCampaignDetails } from '@/app/dashboard/actions'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+import { Pen, Pencil, Save } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+
+export default function EditableCampaignDetails({ campaign, campaignId }) {
+    const [editedCampaign, setEditedCampaign] = useState(campaign)
+    const [isEditingName, setIsEditingName] = useState(false)
+    const [isEditingDescription, setIsEditingDescription] = useState(false)
+    const router = useRouter()
+
+    const handleSave = async (field) => {
+        const updates = { [field]: editedCampaign[field] }
+        const result = await updateCampaignDetails(campaignId, updates)
+        if (result.error) {
+            alert(result.error)
+        } else {
+            setEditedCampaign(result.campaign)
+            router.refresh()
+        }
+        if (field === 'name') setIsEditingName(false)
+        if (field === 'description') setIsEditingDescription(false)
+    }
+
+    return (
+        <div className="flex flex-col gap-8">
+            {isEditingName ? (
+                <span className="flex flex-row gap-2 items-center">
+                    <h1 className="font-bold">Campaign name:</h1>
+                    <Input 
+                        className="w-[400px]" 
+                        value={editedCampaign.name} 
+                        onChange={(e) => setEditedCampaign({ ...editedCampaign, name: e.target.value })} 
+                    />
+                    <Save 
+                        className="w-4 h-4 cursor-pointer hover:scale-110" 
+                        onClick={() => handleSave('name')} 
+                    />
+                </span>
+            ) : (
+                <div className="flex flex-col gap-2">
+                    <span>
+                        <h1 className="font-bold">Campaign name:</h1>
+                        <p className="text-gray-500 text-sm">What is your epic adventure called?</p>
+                    </span>
+                <span className="flex flex-row gap-2 items-center text-sm">
+                    <h2>{editedCampaign.name}</h2>
+                    <Pen 
+                        className="w-4 h-4 cursor-pointer hover:scale-110" 
+                        onClick={() => setIsEditingName(true)} 
+                    /> 
+                </span>
+                </div>
+            )}
+        
+            {isEditingDescription ? (
+                <span className="flex flex-col gap-2">
+                    <Textarea 
+                        className="w-[400px]" 
+                        value={editedCampaign.description} 
+                        onChange={(e) => setEditedCampaign({ ...editedCampaign, description: e.target.value })} 
+                    />
+                    <Save 
+                        className="w-4 h-4 cursor-pointer hover:scale-110" 
+                        onClick={() => handleSave('description')} 
+                    />
+                </span>
+            ) : (
+                <div className="flex flex-col gap-2">
+                    <span>
+                        <h1 className="font-bold">Campaign description:</h1>
+                        <p className="text-gray-500 text-sm">Tell us about your epic adventure. What is your tagli</p>
+                    </span>
+                    <span className="flex flex-row gap-2 items-center">
+                        <p>{editedCampaign.description}</p>
+                    <Pencil 
+                        className="w-4 h-4 cursor-pointer hover:scale-110" 
+                        onClick={() => setIsEditingDescription(true)} 
+                    />
+                </span>
+                </div>
+            )}
+            <div>
+                <h1 className="font-bold">Campaign image</h1>
+                <p className="text-gray-500 text-sm">Upload an image for your campaign.</p>
+            </div>
+        </div>
+    )
+}
