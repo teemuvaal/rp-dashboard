3f81dae0df8ba5d160618f7272c3255f80b011d9
Teemu Vaalasmaa <flipsuk@gmail.com>
2024-12-13 11:29:38 +0200

    fonts for landing page

--- components/Dashboard/CampaignList.jsx
+++ components/Dashboard/CampaignList.jsx
@@ -1,15 +1,23 @@
 import CampaignTile from "./CampaignTile"
 
-export default async function CampaignList({ campaigns }) {
+export default async function CampaignList({ campaigns, user }) {
     if (!campaigns || campaigns.length === 0) {
         return <div>No campaigns found. Are you ready to create one or join your friends?</div>
     }
 
+    const ownerCampaigns = campaigns.filter((campaign) => campaign.owner_id === user.id)
+    const memberCampaigns = campaigns.filter((campaign) => campaign.owner_id !== user.id)
     return (
         <div className="flex flex-col gap-4 p-4">
-            <h2>Your Campaigns</h2>
+            <h2>Your Campaigns (owner)</h2>
             <div className="flex flex-col gap-4">
-                {campaigns.map((campaign) => (
+                {ownerCampaigns.map((campaign) => (
+                    <CampaignTile key={campaign.id} {...campaign} />
+                ))}
+            </div>
+            <h2>Your Campaigns (member)</h2>
+            <div className="flex flex-col gap-4">
+                {memberCampaigns.map((campaign) => (
                     <CampaignTile key={campaign.id} {...campaign} />
                 ))}
             </div>
