04b675a4adebd1059593bbc74d2126cabcf29d8a
Teemu Vaalasmaa <flipsuk@gmail.com>
2025-01-11 22:52:02 +0200

    add features to landingpage

--- components/LandingPage/LandingPageFeatures.jsx
+++ components/LandingPage/LandingPageFeatures.jsx
@@ -1,16 +1,26 @@
 "use client"
 
 import FeatureCard from "./FeatureCard";
-import { NotebookPen, User, Map } from 'lucide-react';
+import { NotebookPen, User, Map, Sparkles, Calendar } from 'lucide-react';
 import { motion } from "framer-motion";
 // List all features here
 
 const features = [
     {
+        title: "AI Assistant",
+        icon: <Sparkles height={20} width={20}/>,
+        children: "Chat with AI assistant to help you design your adventures and recall key information from your notes."
+    },
+    {
         title: "Campaign Notes",
         icon: <NotebookPen height={20} width={20}/>,
         children: "Share your notes, keep them organized and clean up with AI.",
         image: "/productshot_dashboard.png"
+    },
+    {
+        title: "Campaign Calendar",
+        icon: <Calendar height={20} width={20}/>,
+        children: "Manage your sessions, schedules and more with polls."
     },
     {
         title: "Polls",
@@ -20,14 +30,16 @@ const features = [
     {
         title: "Assets",
         icon: <Map height={20} width={20}/>,
-        children: "Share and manage your campaign assets with your players."
-    }
+        children: "Share and manage your campaign assets (characters, locations, etc.) with your players."
+    },
+
+
 ]
 
 
 export default function LandingPageFeatures() {
     return (
-        <div className="flex flex-col items-center justify-center" id="features">
+        <div className="flex flex-col col-span-2 items-center justify-center" id="features">
             <h2
             style={{ fontFamily: 'var(--font-departure-mono)' }}
             className="text-2xl md:text-4xl text-white mb-6"
