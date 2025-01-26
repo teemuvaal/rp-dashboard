d267dce8dde55982b18612c9893089c7a6a98b9a
Teemu Vaalasmaa <flipsuk@gmail.com>
2025-01-26 10:36:03 +0200

    adjust discord login

--- app/login/reset/actions.js
+++ app/login/reset/actions.js
@@ -4,6 +4,6 @@ import { createClient } from '@/utils/supabase/server'
 export async function resetPassword(formData) {
   const supabase = createClient()
   await supabase.auth.resetPasswordForEmail(formData.get('email'), {
-    redirectTo: 'http://localhost:3000/login/reset/new'
+    redirectTo: `${process.env.NEXT_PUBLIC_URL}/login/reset/new`
   })
 }
